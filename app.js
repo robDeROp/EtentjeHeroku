31dba6a5fde25273a522ea1101902c28890df010
robDeROp <79730799+robDeROp@users.noreply.github.com>
2022-01-13 14:59:50 +0100

    push

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyparser = require('body-parser');
 const { response } = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
