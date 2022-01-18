3dbde5e25a8ca5f5356257b98ac947ac8cdc930c
robDeROp <79730799+robDeROp@users.noreply.github.com>
2022-01-18 17:54:12 +0100

    2

--- app.js
+++ app.js
@@ -188,3 +188,44 @@ app.get('/NewOrderLine/:OrderID/:ProdID/:ProdQuantity', function(req, res){ //GE
     res.end(JSON.stringify(data));
   });
 });
+
+/*getting bar order*/
+app.get('/BarOrder', function(req, res){ //GET method to access DB and return results in JSON
+  connection.query('SELECT O.Family_ID, O.Table_ID, P.Description, D.Quantity FROM Orders O JOIN OrderDetails D ON O.ID=D.OrderID JOIN Products P ON D.ProductID = P.ID WHERE P.Category = "Drank" AND O.ID IN ( SELECT MIN(ID) FROM Orders WHERE BarPrint="0" )',
+  function(err, rows, fields){
+    if(err) throw err;
+    var data = [];
+    for(i=0;i<rows.length;i++){
+      data.push(rows[i]);
+    }
+    console.log(JSON.stringify(data));
+    res.end(JSON.stringify(data));
+  });
+});
+/*Change ORDER PRINT STATE*/
+
+app.get('/BarOrderPrinted', function(req, res){ //GET method to access DB and return results in JSON
+  connection.query('UPDATE Orders SET BarPrint= "1" WHERE ID = ""',
+  function(err, rows, fields){
+    if(err) throw err;
+    var data = [];
+    for(i=0;i<rows.length;i++){
+      data.push(rows[i]);
+    }
+    console.log(JSON.stringify(data));
+    res.end(JSON.stringify(data));
+  });
+});
+
+app.get('/KeukenOrderPrinted', function(req, res){ //GET method to access DB and return results in JSON
+  connection.query('UPDATE Orders SET KeukenPrint= "1" WHERE ID = ""',
+  function(err, rows, fields){
+    if(err) throw err;
+    var data = [];
+    for(i=0;i<rows.length;i++){
+      data.push(rows[i]);
+    }
+    console.log(JSON.stringify(data));
+    res.end(JSON.stringify(data));
+  });
+});
