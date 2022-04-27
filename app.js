2adb261d75d802f4ab849b53e89a69bd79a0b4e8
robDeROp <79730799+robDeROp@users.noreply.github.com>
2022-04-27 15:15:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,19 @@ var connection = mysql.createConnection({
   database: 'ID362979_Etentje'
 })
 
+
+app.get('/Companys', function(req, res){ //GET method to access DB and return results in JSON
+  connection.query('SELECT C.Name, C.Email FROM Company C ',
+  function(err, rows, fields){
+    if(err) throw err;
+    var data = [];
+    for(i=0;i<rows.length;i++){
+      data.push(rows[i]);
+    }
+    console.log(JSON.stringify(data));
+    res.end(JSON.stringify(data));
+  });
+});
 app.get('/getMenu/:Editie', function(req, res){ //GET method to access DB and return results in JSON
   connection.query('SELECT Description From Products WHERE Editie_ID = "' + req.params.Editie + '"',
   function(err, rows, fields){
